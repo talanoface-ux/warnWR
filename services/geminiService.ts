45f096c3c4615b56b985b528ecd1e187d65a51c9
talanoface-ux <talanoface@gmail.com>
2025-11-04 03:46:43 +0330

    Update geminiService.ts

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -63,7 +63,7 @@ export const getChatResponse = async (
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        model: "tngtech/deepseek-r1t2-chimera:free", // مدل پیشنهادی بدون فیلتر
+        model: "qwen/qwen3-235b-a22b:free", // مدل پیشنهادی بدون فیلتر
         messages: formattedMessages,
         temperature: 1,
       }),
